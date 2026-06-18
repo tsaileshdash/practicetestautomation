f327aaa4b2e6907ab958b7b4932587b62702b1cf
tsaileshdash <tsaileshdash@example.com>
2026-06-18 16:51:43 +0530

    Configure Allure results folder

--- playwright.config.js
+++ playwright.config.js
@@ -7,8 +7,10 @@ module.exports = {
     headless: true,
     viewport: { width: 1280, height: 720 }
   },
-  reporter: [
+ reporter: [
   ['list'],
-  ['allure-playwright']
+  ['allure-playwright', {
+    outputFolder: 'allure-results'
+  }]
 ]
 };
